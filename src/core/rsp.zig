1d9f5f825692b264adad2d79735c36beebb2910e
Michelle-Marie Schiller <michellemarie.schiller@outlook.com>
2022-01-31 13:57:31 +0100

    Add VU instructions

--- src/core/rsp.zig
+++ src/core/rsp.zig
@@ -16,6 +16,14 @@ const mi = @import("mi.zig");
 
 const InterruptSource = mi.InterruptSource;
 
+/// Clamp 32-bit signed data
+fn clamps32(data: i32) i16 {
+    if (data < -32768) return -32768;
+    if (data >  32767) return  32767;
+
+    return @truncate(i16, data);
+}
+
 /// Sign extend 8-bit data
 fn exts8(data: u8) u32 {
     return @bitCast(u32, @intCast(i32, @bitCast(i8, data)));
@@ -24,6 +32,16 @@ fn exts8(data: u8) u32 {
 /// Sign extend 16-bit data
 fn exts16(data: u16) u32 {
     return @bitCast(u32, @intCast(i32, @bitCast(i16, data)));
+}
+
+/// Get VU byte index
+fn getIdxB(idx: u32) u32 {
+    return 15 - idx;
+}
+
+/// Get VU lane index
+fn getIdxL(idx: u32) u32 {
+    return 7 - idx;
 }
 
 const RSPMemory = enum(u64) {
@@ -57,11 +75,14 @@ const RSPOpcode = enum(u32) {
     ORI   = 0x0D,
     LUI   = 0x0F,
     COP0  = 0x10,
+    COP2  = 0x12,
     LH    = 0x21,
     LW    = 0x23,
     LHU   = 0x25,
     SH    = 0x29,
     SW    = 0x2B,
+    LWC2  = 0x32,
+    SWC2  = 0x3A,
 };
 
 const RSPSpecial = enum(u32) {
@@ -75,6 +96,25 @@ const RSPSpecial = enum(u32) {
 const RSPCOPOpcode = enum(u32) {
     MF = 0x00,
     MT = 0x04,
+};
+
+const RSPCP2Opcode = enum(u32) {
+    COMPUTE = 0x10,
+};
+
+const RSPVULoadOpcode = enum(u32) {
+    LDV = 0x03,
+    LQV = 0x04,
+};
+
+const RSPVUStoreOpcode = enum(u32) {
+    SDV = 0x03,
+    SQV = 0x04,
+};
+
+const RSPVUOpcode = enum(u32) {
+    VMULF = 0x00,
+    VMACF = 0x08,
 };
 
 const RSPAddr = packed struct {
@@ -106,6 +146,49 @@ const RSPStatus = packed struct {
     s7 : bool = false,
 };
 
+const VectorReg = packed union {
+    sLane: [8 ]i16,
+    uLane: [8 ]u16,
+    byte : [16]u8,
+
+    pub fn getSLane(self: VectorReg, idx: u32) i16 {
+        return self.sLane[getIdxL(idx)];
+    }
+
+    pub fn getULane(self: VectorReg, idx: u32) u16 {
+        return self.uLane[getIdxL(idx)];
+    }
+
+    pub fn setSLane(self: *VectorReg, idx: u32, data: i16) void {
+        self.sLane[getIdxL(idx)] = data;
+    }
+
+    pub fn setULane(self: *VectorReg, idx: u32, data: u16) void {
+        self.uLane[getIdxL(idx)] = data;
+    }
+};
+
+const Accumulator = union {
+    sLane: [8]i48,
+    uLane: [8]u48,
+
+    pub fn getSLane(self: Accumulator, idx: u32) i48 {
+        return self.sLane[getIdxL(idx)];
+    }
+
+    pub fn getULane(self: Accumulator, idx: u32) u48 {
+        return self.uLane[getIdxL(idx)];
+    }
+
+    pub fn setSLane(self: *Accumulator, idx: u32, data: i48) void {
+        self.sLane[getIdxL(idx)] = data;
+    }
+
+    pub fn setULane(self: *Accumulator, idx: u32, data: u48) void {
+        self.uLane[getIdxL(idx)] = data;
+    }
+};
+
 const RSPRegs = struct {
     rspAddr  : RSPAddr   = RSPAddr{},
     rspDMALen: RSPDMALen = RSPDMALen{},
@@ -117,12 +200,74 @@ const RSPRegs = struct {
 
     gprs: [32]u32 = undefined,
 
+    vprs: [32]VectorReg = undefined,
+    
+    acc: Accumulator = undefined,
+
     pc : u12 = undefined,
     cpc: u12 = undefined,
     npc: u12 = undefined,
 
     pub fn get(self: RSPRegs, idx: u32) u32 {
         return self.gprs[idx];
+    }
+
+    pub fn getVPR(self: RSPRegs, idx: u32) VectorReg {
+        return self.vprs[idx];
+    }
+
+    pub fn getByte(self: RSPRegs, idx: u32, element: u32) u8 {
+        return self.vprs[idx].byte[getIdxB(element)];
+    }
+
+    pub fn getLane(self: RSPRegs, idx: u32, element: u32) u16 {
+        return self.vprs[idx].uLane[getIdxL(element)];
+    }
+
+    pub fn getVector(self: RSPRegs, idx: u32) u128 {
+        var data: u128 = undefined;
+
+        @memcpy(@ptrCast([*]u8, &data), @ptrCast([*]u8, &vprs[idx].byte), 16);
+
+        return @byteSwap(u128, data);
+    }
+
+    pub fn broadcast(self: RSPRegs, idx: u32, e: u32) VectorReg {
+        const v = self.vprs[idx];
+
+        var vBroadcast: VectorReg = undefined;
+
+        var mask: u32 = undefined;
+
+        switch (e) {
+             0,1 => mask = 0x76543210,
+             2   => mask = 0x66442200,
+             3   => mask = 0x77553311,
+             4   => mask = 0x44440000,
+             5   => mask = 0x55551111,
+             6   => mask = 0x66662222,
+             7   => mask = 0x77773333,
+             8   => mask = 0x00000000,
+             9   => mask = 0x11111111,
+            10   => mask = 0x22222222,
+            11   => mask = 0x33333333,
+            12   => mask = 0x44444444,
+            13   => mask = 0x55555555,
+            14   => mask = 0x66666666,
+            15   => mask = 0x77777777,
+            else => @panic("invalid broadcast modifier"),
+        }
+
+        vBroadcast.setULane(0, v.getULane((mask >>  0) & 0xF));
+        vBroadcast.setULane(1, v.getULane((mask >>  4) & 0xF));
+        vBroadcast.setULane(2, v.getULane((mask >>  8) & 0xF));
+        vBroadcast.setULane(3, v.getULane((mask >> 12) & 0xF));
+        vBroadcast.setULane(4, v.getULane((mask >> 16) & 0xF));
+        vBroadcast.setULane(5, v.getULane((mask >> 20) & 0xF));
+        vBroadcast.setULane(6, v.getULane((mask >> 24) & 0xF));
+        vBroadcast.setULane(7, v.getULane((mask >> 28) & 0xF));
+
+        return vBroadcast;
     }
 
     pub fn getCP0(self: RSPRegs, idx: u32) u32 {
@@ -156,6 +301,24 @@ const RSPRegs = struct {
         self.gprs[idx] = data;
 
         self.gprs[0] = 0;
+    }
+
+    pub fn setByte(self: *RSPRegs, idx: u32, element: u32, data: u8) void {
+        self.vprs[idx].byte[getIdxB(element)] = data;
+    }
+
+    pub fn setLane(self: *RSPRegs, idx: u32, element: u32, data: u16) void {
+        self.vprs[idx].uLane[getIdxL(element)] = data;
+    }
+
+    pub fn setSLane(self: *RSPRegs, idx: u32, element: u32, data: i16) void {
+        self.vprs[idx].sLane[getIdxL(element)] = data;
+    }
+
+    pub fn setVector(self: *RSPRegs, idx: u32, data: u128) void {
+        const data_ = @byteSwap(u128, data);
+
+        @memcpy(@ptrCast([*]u8, &vprs[idx].byte), @ptrCast([*]const u8, &data_), 16);
     }
 
     pub fn setCP0(self: RSPRegs, idx: u32, data: u32) void {
@@ -217,6 +380,26 @@ fn getSa(instr: u32) u32 {
 fn getTarget(instr: u32) u32 {
     return (instr << 2) & 0xFFF_FFFF;
 }
+
+fn getFunct(instr: u32) u32 {
+    return instr & 0x3F;
+}
+
+fn getElement(instr: u32) u32 {
+    return (instr >> 7) & 0xF;
+}
+
+fn getBroadcastMod(instr: u32) u32 {
+    return (instr >> 21) & 0xF;
+}
+
+fn getOffset(instr: u32, comptime n: comptime_int) u32 {
+    return @bitCast(u32, @intCast(i32, @bitCast(i7, @truncate(u7, instr)))) << n;
+}
+
+const getVd = getRd;
+const getVs = getRs;
+const getVt = getRt;
 
 pub fn read8(pAddr: u64) u8 {
     var data: u8 = undefined;
@@ -501,11 +684,53 @@ fn decodeInstr(instr: u32) void {
                 }
             }
         },
+        @enumToInt(RSPOpcode.COP2 ) => {
+            switch (getRs(instr)) {
+                @enumToInt(RSPCOPOpcode.MT) => iMTC(instr, 2),
+                @enumToInt(RSPCP2Opcode.COMPUTE) ... @enumToInt(RSPCP2Opcode.COMPUTE) + 0xF => {
+                    switch (getFunct(instr)) {
+                        @enumToInt(RSPVUOpcode.VMULF) => iVMULF(instr),
+                        @enumToInt(RSPVUOpcode.VMACF) => iVMACF(instr),
+                        else => {
+                            warn("[RSP] Unhandled COP2 function {X}h ({X}h) @ {X}h.", .{getFunct(instr), instr, rspRegs.cpc});
+
+                            @panic("unhandled RSP instruction");
+                        }
+                    }
+                },
+                else => {
+                    warn("[RSP] Unhandled COP2 opcode {X}h ({X}h) @ {X}h.", .{getRs(instr), instr, rspRegs.cpc});
+
+                    @panic("unhandled RSP instruction");
+                }
+            }
+        },
         @enumToInt(RSPOpcode.LH   ) => iLH   (instr),
         @enumToInt(RSPOpcode.LW   ) => iLW   (instr),
         @enumToInt(RSPOpcode.LHU  ) => iLHU  (instr),
         @enumToInt(RSPOpcode.SH   ) => iSH   (instr),
         @enumToInt(RSPOpcode.SW   ) => iSW   (instr),
+        @enumToInt(RSPOpcode.LWC2 ) => {
+            switch (getRd(instr)) {
+                @enumToInt(RSPVULoadOpcode.LDV) => iLDV(instr),
+                @enumToInt(RSPVULoadOpcode.LQV) => iLQV(instr),
+                else => {
+                    warn("[RSP] Unhandled VU load opcode {X}h ({X}h) @ {X}h.", .{getRd(instr), instr, rspRegs.cpc});
+
+                    @panic("unhandled RSP instruction");
+                }
+            }
+        },
+        @enumToInt(RSPOpcode.SWC2 ) => {
+            switch (getRd(instr)) {
+                @enumToInt(RSPVUStoreOpcode.SDV) => iSDV(instr),
+                else => {
+                    warn("[RSP] Unhandled VU store opcode {X}h ({X}h) @ {X}h.", .{getRd(instr), instr, rspRegs.cpc});
+
+                    @panic("unhandled RSP instruction");
+                }
+            }
+        },
         else => {
             warn("[RSP] Unhandled instruction {X}h ({X}h) @ {X}h.", .{opcode, instr, rspRegs.cpc});
 
@@ -642,6 +867,28 @@ fn iJR(instr: u32) void {
     if (isDisasm) info("[RSP] JR ${}; PC = {X}h", .{rs, target});
 }
 
+/// LDV - Load Double into Vector register
+fn iLDV(instr: u32) void {
+    const base = getRs(instr);
+    const vt   = getVt(instr);
+
+    const offset = getOffset(instr, 3);
+
+    const addr = rspRegs.get(base) +% offset;
+
+    var data = readDMEM(u64, addr);
+
+    const element  = getElement(instr);
+    var   element_ = element;
+    while (element_ < (element + 8) and element_ < 16) : (element_ += 1) {
+        rspRegs.setByte(vt, element_, @truncate(u8, data));
+
+        data >>= 8;
+    }
+
+    if (isDisasm) info("[RSP] LDV ${}[{}], ${X}({})", .{vt, element, base, @bitCast(i32, offset)});
+}
+
 /// LH - Load Halfword
 fn iLH(instr: u32) void {
     const imm = exts16(getImm16(instr));
@@ -668,6 +915,27 @@ fn iLHU(instr: u32) void {
     rspRegs.set(rt, @intCast(u32, readDMEM(u16, addr)));
 
     if (isDisasm) info("[RSP] LHU ${}, ${}({}); ${} = ({X}h) = {X}h", .{rt, base, @bitCast(i32, imm), rt, addr, rspRegs.get(rt)});
+}
+
+/// LQV - Load Quad into Vector register
+fn iLQV(instr: u32) void {
+    const base = getRs(instr);
+    const vt   = getVt(instr);
+
+    const offset = getOffset(instr, 4);
+
+    const addr = rspRegs.get(base) +% offset;
+
+    var data = readDMEM(u128, addr);
+
+    var i: u32 = 0;
+    while (i < (addr & 15)) : (i += 1) {
+        rspRegs.setByte(vt, i, @truncate(u8, data));
+
+        data >>= 8;
+    }
+
+    if (isDisasm) info("[RSP] LQV ${}[0], ${X}({})", .{vt, base, @bitCast(i32, offset)});
 }
 
 /// LUI - Load Upper Immediate
@@ -720,6 +988,10 @@ fn iMTC(instr: u32, comptime copN: comptime_int) void {
 
     if (copN == 0) {
         rspRegs.setCP0(rd, data);
+    } else if (copN == 2) {
+        const lane = getElement(instr) >> 1;
+
+        rspRegs.setLane(rd, lane, @truncate(u16, data));
     } else {
         warn("[RSP] Unhandled Coprocessor {}.", .{copN});
 
@@ -739,6 +1011,31 @@ fn iORI(instr: u32) void {
     rspRegs.set(rt, rspRegs.get(rs) | imm);
 
     if (isDisasm) info("[RSP] ORI ${}, ${}, {X}h; ${} = {X}h", .{rt, rs, imm, rt, rspRegs.get(rt)});
+}
+
+/// SDV - Store Double from Vector register
+fn iSDV(instr: u32) void {
+    const base = getRs(instr);
+    const vt   = getVt(instr);
+
+    const offset = getOffset(instr, 3);
+
+    const addr = rspRegs.get(base) +% offset;
+
+    var data: u64 = 0;
+
+    const element = getElement(instr);
+
+    var i: u32 = 0;
+    while (i < 8) : (i += 1) {
+        data |= @intCast(u64, rspRegs.getByte(vt, (element + i) & 31));
+
+        data <<= 8;
+    }
+
+    writeDMEM(u64, addr, data);
+
+    if (isDisasm) info("[RSP] SDV ${}[{}], ${X}({})", .{vt, element, base, @bitCast(i32, offset)});
 }
 
 /// SH - Store Halfword
@@ -795,6 +1092,52 @@ fn iSW(instr: u32) void {
     writeDMEM(u32, addr, rspRegs.get(rt));
 
     if (isDisasm) info("[RSP] SW ${}, ${}({}); ({X}h) = {X}h", .{rt, base, @bitCast(i32, imm), addr, rspRegs.get(rt)});
+}
+
+/// VMACF - Vector Multiply and ACcumulate of signed Fractions
+fn iVMACF(instr: u32) void {
+    const vd = getVd(instr);
+    const vs = getVs(instr);
+    const vt = getVt(instr);
+
+    const e = getBroadcastMod(instr);
+
+    const s = rspRegs.getVPR(vs);
+    const t = rspRegs.broadcast(vt, e);
+
+    var i: u32 = 0;
+    while (i < 8) : (i += 1) {
+        const prod = @intCast(u32, s.getULane(i)) * @intCast(u32, t.getULane(i)) * 2;
+
+        rspRegs.acc.setSLane(i, rspRegs.acc.getSLane(i) + @intCast(i48, @bitCast(i32, prod)));
+
+        rspRegs.setSLane(vd, i, clamps32(@truncate(i32, rspRegs.acc.getSLane(i) >> 16)));
+    }
+
+    if (isDisasm) info("[RSP] VMACF ${}, ${}, ${}[{}]", .{vd, vs, vt, e});
+}
+
+/// VMULF - Vector MULtiply of signed Fractions
+fn iVMULF(instr: u32) void {
+    const vd = getVd(instr);
+    const vs = getVs(instr);
+    const vt = getVt(instr);
+
+    const e = getBroadcastMod(instr);
+
+    const s = rspRegs.getVPR(vs);
+    const t = rspRegs.broadcast(vt, e);
+
+    var i: u32 = 0;
+    while (i < 8) : (i += 1) {
+        const prod = @intCast(u32, s.getULane(i)) * @intCast(u32, t.getULane(i)) * 2;
+
+        rspRegs.acc.setSLane(i, @intCast(i48, @bitCast(i32, prod + 0x8000)));
+
+        rspRegs.setSLane(vd, i, clamps32(@truncate(i32, rspRegs.acc.getSLane(i) >> 16)));
+    }
+
+    if (isDisasm) info("[RSP] VMULF ${}, ${}, ${}[{}]", .{vd, vs, vt, e});
 }
 
 pub fn step() void {
